71bad2c7f4b0ea8527cc966f4908f193e69e1c65
jg-Harshini <jgharshini05@gmail.com>
2025-09-17 13:16:47 +0530

    minor change

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
             'journal_mode' => null,
             'synchronous' => null,
         ],
-
+       
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
